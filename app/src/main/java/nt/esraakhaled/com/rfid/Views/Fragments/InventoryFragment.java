7e4087128974439fafdff36a4142e2f0d92438bd
abdelrahman68233@gmail.com <boodyc.ro11>
2017-08-20 13:31:12 +0200

    add inventory

--- app/src/main/java/nt/esraakhaled/com/rfid/Views/Fragments/InventoryFragment.java
+++ app/src/main/java/nt/esraakhaled/com/rfid/Views/Fragments/InventoryFragment.java
@@ -3,6 +3,7 @@ package nt.esraakhaled.com.rfid.Views.Fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
@@ -14,6 +15,7 @@ import java.util.ArrayList;
 
 import nt.esraakhaled.com.rfid.Controllers.Adapters.BaseAdapter;
 import nt.esraakhaled.com.rfid.Controllers.Interfaces.UHFReaderDelegate;
+import nt.esraakhaled.com.rfid.Controllers.Sensors.UHFReader;
 import nt.esraakhaled.com.rfid.Models.BaseAdapterItem;
 import nt.esraakhaled.com.rfid.R;
 
@@ -21,6 +23,7 @@ import nt.esraakhaled.com.rfid.R;
  * A simple {@link Fragment} subclass.
  */
 public class InventoryFragment extends Fragment implements UHFReaderDelegate{
+     boolean isFirstClick=true;
 
     ArrayList<BaseAdapterItem> tags;
     BaseAdapter adapter;
@@ -37,8 +40,8 @@ public class InventoryFragment extends Fragment implements UHFReaderDelegate{
         tags=new ArrayList<>();
         adapter = new BaseAdapter(getActivity(), BaseAdapter.ListItemType.Single,tags,false);
         return inflater.inflate(R.layout.base_fragment_list, container, false);
-    }
 
+    }
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
@@ -50,8 +53,34 @@ public class InventoryFragment extends Fragment implements UHFReaderDelegate{
         recyclerView.setAdapter(adapter);
 
 
-    }
+        view.findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    if (isFirstClick) {
+                        UHFReader.getInstange(null).subscribeEPCRead(InventoryFragment.this);
+                        UHFReader.getInstange(null).startReading();
 
+                        view.setTag("start");
+                        ((FloatingActionButton) view).setImageResource(R.drawable.stop);
+                    } else {
+                        UHFReader.getInstange(null).stopReading();
+                        UHFReader.getInstange(null).unSubscribeEPCRead(InventoryFragment.this);
+                        view.setTag("stop");
+                        ((FloatingActionButton) view).setImageResource(R.drawable.play);
+
+                    }
+
+                    isFirstClick = !isFirstClick;
+
+                }
+            });
+        view.findViewById(R.id.floatingActionButton).setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+    }
     @Override
     public void tagEPCRead(final String epc) {
 
@@ -72,7 +101,9 @@ public class InventoryFragment extends Fragment implements UHFReaderDelegate{
     }
 
     @Override
-    public void tagEPCDataRead(String data) {
+    public void tagEPCDataRead(String epc, String data) {
 
     }
+
+
 }
